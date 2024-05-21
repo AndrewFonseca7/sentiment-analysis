b7b8ba811f4917b00cf6d673bd7f19ccf71eaa30
Andrew Fonseca <andrew.fonseca7@hotmail.com>
2024-05-21 17:45:38 -0500

    refactor: Update environment variables and cache configuration
    The code changes update the environment variables in the .env.example file to include new values for ENV, PORT, GOOGLE_APPLICATION_CREDENTIALS, MONGODB_URI, ELASTICSEARCH_HOST, REDIS_HOST, REDIS_PORT, CACHE_TTL, and CACHE_SIZE. This ensures that the application is properly configured with the required values.
    
    Additionally, the docker-compose.yml file is updated to include the CACHE_TTL and CACHE_SIZE environment variables for the redis service. This ensures that the caching functionality is configured with the correct values.
    
    Finally, the app.module.ts file is modified to use the ConfigModule and ConfigService from the @nestjs/config package for retrieving the environment variables. This improves the configuration handling and allows for easier access to the environment variables throughout the application.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,23 +3,29 @@ import { AppController } from './app.controller';
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import Logger from './logger/logger';
-import { CacheModule, CacheModuleOptions } from '@nestjs/cache-manager';
+import { CacheModule } from '@nestjs/cache-manager';
 import { AppCacheModule } from './cache/cache.module';
 import * as redisStore from 'cache-manager-ioredis';
 import { SentimentModule } from './sentiment/sentiment.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     DatabaseModule,
     CacheModule.registerAsync({
-      useFactory: (): CacheModuleOptions => ({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
         store: redisStore,
-        host: process.env.REDIS_HOST || 'localhost',
-        port: parseInt(process.env.REDIS_PORT) || 6379,
-        ttl: 600,
-        max: 10,
+        host: configService.get<string>('REDIS_HOST'),
+        port: configService.get<number>('REDIS_PORT'),
+        ttl: configService.get<number>('CACHE_TTL') | 60,
+        max: configService.get<number>('CACHE_SIZE') | 1000,
       }),
       isGlobal: true,
+      inject: [ConfigService],
     }),
     AppCacheModule,
     SentimentModule,
